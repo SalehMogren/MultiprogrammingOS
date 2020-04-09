9d25c7106cbc4978734ff9a801427f2358d16cc2
sz4m <saleh0sm2@gmail.com>
2020-04-09 15:55:12 +0300

    aziz check

--- src/OS.java
+++ src/OS.java
@@ -1,3 +1,16 @@
+/*
+ * Output 
+ * Process ID 
+ * Time when it was loaded into the ready queue ( First time entered the ready queue )
+ * Total spent time in the cpu ( counter every time entered cpu)
+ * Number of times Reformed an IO ( Counter every time there is an IO)
+ * Total spent time performing an IO ( collect all time doing IO)
+ * Number of times was in the Waiting queue ( Counter for waiting)
+ * Number of times was preempted( counter for permeated)
+ * Time it terminated or was Killed ( When ? Using Clock)
+ * Its Final State : Killed or Terminated 
+ * CPU Utilization
+ */
 
 public class OS {
 
--- src/RAM.java
+++ src/RAM.java
@@ -1,4 +1,9 @@
-
+/*This Class Represent The RAM
+ * Its Operations will be :
+ * Load the jobQueue to ready Queue or to waiting queue if it can not be added .
+ * Clear The Largest Process in  the memory when there is a deadlock 
+ * 
+ */
 public class RAM {
 
 	
