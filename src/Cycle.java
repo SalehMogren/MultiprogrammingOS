7384bd8a1e70dcb2f7955b0b0681e8031388ca1e
sz4m <saleh0sm2@gmail.com>
2020-04-11 14:04:51 +0300

    Deleted extra files

--- src/Cycle.java
+++ src/Cycle.java
@@ -3,19 +3,19 @@ public class Cycle {
 	private int cpuBurst;
 	private int memory;
 	private int IOBurst;
-	private int arrivalTime;
+	
 	
 	public Cycle() {
 		this.cpuBurst = 0;
 		this.IOBurst = 0 ;
 		this.memory = 0 ;
-		this.arrivalTime=0;
+		
 	}
 	public Cycle(int cpuBurst , int memory , int IOBurst , int arrivalTime){
 		this.cpuBurst = cpuBurst;
 		this.memory = memory;
 		this.IOBurst = IOBurst;
-		this.arrivalTime=arrivalTime;
+		
 	}
 	// setters & getters
 	
@@ -40,12 +40,6 @@ public class Cycle {
 		IOBurst = iOBurst;
 	}
 	
-	public int getArrivalTime() {
-		return arrivalTime;
-	}
-	public void setArrivalTime(int arrivalTime) {
-		this.arrivalTime = arrivalTime;
-	}
 	
 	
 
