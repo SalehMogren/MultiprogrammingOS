92b46e1ae3487fa93f515fc777a3e94d020d034a
sz4m <saleh0sm2@gmail.com>
2020-04-16 22:55:14 +0300

    CPU notes

--- src/CPU.java
+++ src/CPU.java
@@ -1,6 +1,19 @@
-
+/*
+ * 1- ReadyQueue - Converting to queue sorted on ( arr,cpuBurst) WaitingQueue - converting to Queue
+2- general way is process will be executed each cpuBurst (time unit) when it reach 0 we will then preform the IO burts that will stop everything until its finished
+	add it again to the ready queue update cpuBurst,IOBurst and add Memory
+3 - Special case when new shortest process enters the ready , create PQ premetidProcess 
+4- Each time unit we compare with the shortest process min(premetidProcess ,next new Process)
+5- Until ram is empty
+ */
 public class CPU {
-	int counter;
-	/////jnkuhiyuhiuuih
+	int totoaCPUTime,totalIOtime;
+	PQ<PCB> waitingQueue,readyQueue;
+	
+	
+	
+	
+	//Based on the arrival time , we execute first process then check the 
+	//
 	
 }
